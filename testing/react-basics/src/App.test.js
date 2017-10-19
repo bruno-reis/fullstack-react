7e8ffccb7a23942be40683959c4323e8f03812c1
bruno-reis <13205396+bruno-reis@users.noreply.github.com>
2017-10-19 17:40:40 -0200

    added basic Jest tests

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('App', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('should have the `th` "Items"', () => {
+    expect(
+        wrapper.contains(<th>Items</th>)
+    ).toBe(true);
+  })
+
+  it('should have a button elem', () => {
+    expect(
+        wrapper.containsMatchingElement(<button>Add item</button>)
+    ).toBe(true)
+  })
+
+  it('should have a button elem disabled', () => {
+    expect(
+        wrapper.containsMatchingElement(<button disabled={true}>Add item</button>)
+    ).toBe(true)
+  })
+
+  it('should have a input elem', () => {
+    expect(
+        wrapper.containsMatchingElement(<input />)
+    ).toBe(true)
+  })
+
+  it('should have a button elem disabled ver2', () => {
+    const button = wrapper.find('button').first()
+    expect(button.props().disabled).toBe(true)
+  })
+})
